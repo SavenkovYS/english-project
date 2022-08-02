5e840d85fad6984c4027f8bae3672aa58dbb0a05
savenkov <savenkov@cmd.su>
2022-08-02 16:23:22 +0300

    fixing validation issues

--- src/frontend/src/app/index.vue
+++ src/frontend/src/app/index.vue
@@ -1,36 +1,11 @@
 <template>
   <div class="app">
-    <loading-spinner v-if="isLoading" />
-    <Routing v-else />
+    <Routing />
   </div>
 </template>
 
 <script setup lang="ts">
 import { Routing } from '@/pages';
-import { onBeforeMount, ref } from 'vue';
-import { useAuth } from '@/processes/auth/model/auth';
-import LoadingSpinner from '@/shared/design/UI/LoadingSpinner.vue';
-
-const auth = useAuth();
-
-const isLoading = ref(false);
-
-async function checkUserAuth() {
-  isLoading.value = true;
-  if (!auth.isLoggedIn) {
-    try {
-      await auth.checkAuth();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      isLoading.value = false;
-    }
-  }
-}
-
-onBeforeMount(() => {
-  checkUserAuth();
-});
 </script>
 
 <style>
